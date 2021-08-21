6669eb2e10f64f92740b9e718cd454539db06d8a
Tomohiro Kumagai <tomohiro@ez-style.jp>
2021-08-22 06:55:43 +0900

    Conforms `URLSchemeActionError` to `CustomDebugStringConvertible`.

--- Sources/Sky-AppKit/URLScheme/URLSchemeActionError.swift
+++ Sources/Sky-AppKit/URLScheme/URLSchemeActionError.swift
@@ -13,3 +13,18 @@ public enum URLSchemeActionError : Error {
     case invalidURL(URL, description: String)
     case invocationFailure(description: String)
 }
+
+extension URLSchemeActionError : CustomDebugStringConvertible {
+    
+    public var debugDescription: String {
+        
+        switch self {
+        
+        case .invalidURL(let url, description: let description):
+            return "Invalid URL: \(description) (\(url.description))"
+            
+        case .invocationFailure(description: let description):
+            return "Invocation failure: \(description)"
+        }
+    }
+}
