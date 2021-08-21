5c09dff89577ba41eb6f5ba2048303ec89a30190
Tomohiro Kumagai <tomohiro@ez-style.jp>
2021-08-21 15:22:51 +0900

    Updates the dependency information of this package.

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Sky",
-            dependencies: ["Sky-AppKit"]),
+            dependencies: ["Sky-AppKit", "Ocean"]),
         .target(
             name: "Sky-AppKit",
             dependencies: ["Ocean"]),
