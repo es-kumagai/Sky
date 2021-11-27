388415c7960a4590ec1beecb8d8bdc4697bae4f9
Tomohiro Kumagai <tomohiro@ez-style.jp>
2021-11-27 16:37:23 +0900

    Support kushview Element.

---
+++ Sources/Sky-AppKit/OwningAppliation/Applications/KushviewApplications.swift
@@ -0,0 +1,11 @@
+//
+//  KushviewApplications.swift
+//  
+//
+//  Created by Tomohiro Kumagai on 2021/06/19.
+//
+
+public extension OwningApplication {
+    
+    static let kushviewElement = OwningApplication(identifier: "new.kushview.Element")
+}
