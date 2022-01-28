1b3eba4a861665cc19e216e9552ec9adb7228777
Tomohiro Kumagai <tomohiro@ez-style.jp>
2022-01-28 18:56:32 +0900

    Add an owning application information.

---
+++ Sources/Sky-AppKit/OwningAppliation/Applications/NotionApplications.swift
@@ -0,0 +1,11 @@
+//
+//  NotionApplications.swift
+//  
+//
+//  Created by Tomohiro Kumagai on 2021/06/19.
+//
+
+public extension OwningApplication {
+    
+    static let notion = OwningApplication(identifier: "notion.id")
+}
