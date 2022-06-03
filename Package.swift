7043e4857c0dbbc3f57d76ae66401749c59904b5
Tomohiro Kumagai <tomo@ez-net.jp>
2022-06-03 18:43:22 +0900

    Make the library type not specified.

--- Package.swift
+++ Package.swift
@@ -12,11 +12,9 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Sky",
-            type: .dynamic,
             targets: ["Sky"]),
         .library(
             name: "Sky-AppKit",
-            type: .dynamic,
             targets: ["Sky-AppKit"]),
     ],
     dependencies: [
