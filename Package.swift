e8f06a8a3217a22055549f6892f4bab647523ef5
Tomohiro Kumagai <tomo@ez-net.jp>
2023-08-19 00:23:29 +0900

    Updated to support macOS version v11 or higher and modified the branch name of the dependent package `Swim`.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Sky",
     platforms: [
-        .macOS(.v10_15)
+        .macOS(.v11)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -22,7 +22,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/es-kumagai/Swim", .branch("master")),
+        .package(url: "https://github.com/es-kumagai/Swim", .branch("main")),
         .package(url: "https://github.com/es-kumagai/Ocean", .branch("master")),
     ],
     targets: [
